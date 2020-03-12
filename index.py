a1a3c682a55cefb2354b62d42fb1c3291a2094bc
sometimesvlad <requestvnm@gmail.com>
2020-03-12 14:27:14 +0300

    Update index.py & Manual Added

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 from scanner import scanWrapper
 from reverseshServer import reverseshWrapper
 
-
+print(" ")
+print("Welcome to Networker - penetration testing tool for combat situations")
+print(" ") 
 print("\t[1] Reverse shell server")
 print("\t[2] Port Scanner")
 print("\t[3] Help")
@@ -12,7 +14,7 @@ if __name__ == "__main__":
     while True:
         action = input("[*] Load module >> ")
         if action == "1":
-            print("[!] TCP connection module activated...")
+            print("[!] Reverse shell module activated...")
             reverseshWrapper()
             break
         elif action == "2":
@@ -20,7 +22,13 @@ if __name__ == "__main__":
             scanWrapper()
             break
         elif action == "3":
-            print("Help")
+            print("[!] Manual module activated...")
+            print(" ")
+            print("\t\t\t\t\t*** MANUAL ***")
+            print(" ")
+            print("REVERSE SHELL MODULE - set your public IP/port to listen for incoming connections,\n                       then start backdoor.py on target machine to get reverse shell")
+            print("PORT SCANNER MODULE  - poor Nmap, set scan type [1] Single port scan or [2] List port scan \n                       to get some information about target machine")
             break
         else:
-            print("[!] Error")
+            print("[!] Unrecognized module choosed")
+            continue
